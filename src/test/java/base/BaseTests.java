6fbb10f6a96c2a3c11e0a22bc06045cbd1348e92
mwaz <amby.mwaz@gmail.com>
2020-04-28 10:58:25 +0300

    fix: email change

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,14 +1,10 @@
 package base;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import pages.HomePage;
-
-import java.util.List;
 
 public class BaseTests {
 
