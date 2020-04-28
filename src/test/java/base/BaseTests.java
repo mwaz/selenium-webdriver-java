d167361b68dfb9845349469d6890653c91fbeb3d
mwaz <waweru.mwaura@allangray.co.za>
2020-04-28 07:54:27 +0300

    fix: test base case

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -4,38 +4,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import pages.HomePage;
 
 import java.util.List;
 
 public class BaseTests {
 
 	private WebDriver driver;
+	protected HomePage homePage;
 
+	@BeforeClass
 	public void setUp(){
 		System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
 		driver = new ChromeDriver();
 		driver.get(Links.URL);
-
-
-		List<WebElement> links  = driver.findElements(By.tagName("a"));
-		System.out.println(links.size());
-
-
-		driver.findElement(By.linkText("Shifting Content")).click();
-
-		driver.findElement(By.linkText("Example 1: Menu Element")).click();
-
-		WebElement menu = driver.findElement(By.tagName("li"));
-//		System.out.println(menu.size() + " menu links");
-
-		System.out.println(driver.getTitle());
-		driver.quit();
+		homePage = new HomePage(driver);
 
 	}
-
-	public static void main(String args[]){
-		BaseTests test = new BaseTests();
-		test.setUp();
+	@AfterClass
+	public  void TearDown(){
+		driver.quit();
 	}
 
 }
