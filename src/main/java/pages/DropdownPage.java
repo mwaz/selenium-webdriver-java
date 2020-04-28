fb4466c98717b7c1cdc08c41e3c7735f3618e4f5
mwaz <waweru.mwaura@allangray.co.za>
2020-04-28 07:53:41 +0300

    fix: dropdown page

--- src/main/java/pages/DropdownPage.java
+++ src/main/java/pages/DropdownPage.java
@@ -1,5 +1,31 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
 public class DropdownPage {
+	private WebDriver driver;
+	private By  dropdown = By.id("dropdown");
+
+	public DropdownPage(WebDriver driver){
+		this.driver = driver;
+	}
+
+	public  void selectFromDropDown(String option){
+		Select dropdownElement = new Select(driver.findElement(dropdown));
+		dropdownElement.selectByVisibleText(option);
+
+	}
+
+	private Select findDropDownElement(){
+		return new Select(driver.findElement(dropdown));
+	}
+
+	public List<String> getSelectedOption(){
+		findDropDownElement().getAllSelectedOptions();
+	}
 
 }
