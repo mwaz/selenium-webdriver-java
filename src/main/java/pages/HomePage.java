5941b151d1c55e091fc9c7771d3f5b8cc179185a
mwaz <waweru.mwaura@allangray.co.za>
2020-04-28 07:52:51 +0300

    fix: add homepage

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,5 +1,28 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class HomePage {
+	private WebDriver driver;
+
+	public HomePage(WebDriver driver){
+		this.driver = driver;
+	}
+
+	public void clickLink (String linkText){
+		driver.findElement(By.linkText(linkText)).click();
+	}
+
+	public LoginPage clickFormAuthentication(){
+		clickLink("Form Authentication");
+		return  new LoginPage(driver);
+	}
+
+	public DropdownPage clickDropdown (){
+		clickLink("Dropdown");
+		return  new DropdownPage(driver);
+	}
+
 
 }
