b5acc501927200ea636bb621cbc55703f82fc156
mwaz <amby.mwaz@gmail.com>
2020-04-29 19:55:09 +0300

    fix: dropdown testing in Java

---
+++ src/test/java/dropdown/DropdownTest.java
@@ -0,0 +1,22 @@
+package dropdown;
+
+import base.BaseTests;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class DropdownTest  extends BaseTests {
+
+	@Test
+	public void  testSelectOption(){
+		String option = "Option 1";
+
+		var dropDropdownPage = homePage.clickDropdown();
+		dropDropdownPage.selectFromDropDown(option);
+		var selectedOptions = dropDropdownPage.getSelectedOptions();
+
+		assertEquals(selectedOptions.size(), 1, "Incorrect number of selections");
+		assertTrue(selectedOptions.contains(option), "Option not selected");
+
+	}
+
+}
