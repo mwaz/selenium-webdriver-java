2d661a045fa9bc34d88b7a8079f298f5c206886e
mwaz <amby.mwaz@gmail.com>
2020-05-05 07:52:45 +0300

    fix: horizontal sliders

--- src/main/java/pages/HorizontalSliderPage.java
+++ src/main/java/pages/HorizontalSliderPage.java
@@ -1,5 +1,24 @@
 package pages;
 
-public class HorizontalSliderPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 
+public class HorizontalSliderPage {
+	private WebDriver driver;
+	private By slider = By.tagName("input");
+	private By sliderValue = By.id("range");
+
+	public HorizontalSliderPage(WebDriver driver){
+		this.driver = driver;
+	}
+
+	public void moveSlider(int moveSteps){
+		for(var i =1; i<=moveSteps; i++){
+			driver.findElement(slider).sendKeys(Keys.chord(Keys.ALT, Keys.ARROW_RIGHT));
+		}
+	}
+	public String getSliderValue(){
+		return  driver.findElement(sliderValue).getText();
+	}
 }
