e7f87881a8bdc37a79f9ae2659b6db1e855e4f92
mwaz <amby.mwaz@gmail.com>
2020-05-05 06:49:58 +0300

    fix: hover on elements on a page

--- src/main/java/pages/HoversPage.java
+++ src/main/java/pages/HoversPage.java
@@ -1,5 +1,61 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.List;
+
 public class HoversPage {
+	public WebDriver driver;
+	private By figureBox = By.className("figure");
+	private By boxCaption = By.className("figcaption");
+	private By header = By.tagName("h5");
+	private By link = By.tagName("a");
+
+	/**
+	 * Constructor method that takes in webdriver
+	 * @param driver
+	 */
+	public HoversPage(WebDriver driver){
+		this.driver = driver;
+
+	}
+
+	/**
+	 *
+	 * @param index starts at 1
+	 */
+	public FigureCaption  hoverOverFigure(int index){
+		WebElement figure = driver.findElements(figureBox).get(index - 1);
+		Actions actions = new Actions((driver));
+		actions.moveToElement(figure).perform();
+		return new FigureCaption(figure.findElement(boxCaption));
+	}
+
+	public class FigureCaption{
+
+		private WebElement caption;
+
+		public FigureCaption(WebElement caption){
+			this.caption = caption;
+		}
+
+		public boolean isCaptionDisplayed(){
+			return caption.isDisplayed();
+		}
+
+		public String getTitle(){
+			return caption.findElement(header).getText();
+		}
+
+		public String getLink(){
+			return caption.findElement(link).getAttribute("href");
+		}
+		public String getLinkText(){
+			return caption.findElement(link).getText();
+		}
+	}
 
 }
