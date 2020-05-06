d2cbdee66a5791a4a40ee1ba30874a5f670a383b
mwaz <amby.mwaz@gmail.com>
2020-05-06 13:01:28 +0300

    fix: add alerts tests

--- src/test/java/alerts/AlertTests.java
+++ src/test/java/alerts/AlertTests.java
@@ -1,5 +1,17 @@
 package alerts;
 
-public class AlertTests {
+import base.BaseTests;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class AlertTests extends BaseTests {
+	@Test
+	public void testAlerts(){
+		var alertsPage = homePage.clickJavaScriptAlerts();
+		alertsPage.triggerAlert();
+		alertsPage.acceptAlert();
+		assertEquals(alertsPage.getResult(), "You successfuly clicked an alert", "Results text incorrect");
+	}
 
 }
