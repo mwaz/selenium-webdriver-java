bc6b510945236ca283a843695939106171c8f9c5
mwaz <amby.mwaz@gmail.com>
2020-05-06 13:28:42 +0300

    fix: iframe test

--- src/test/java/frames/FrameTest.java
+++ src/test/java/frames/FrameTest.java
@@ -1,5 +1,21 @@
 package frames;
 
-public class FrameTest {
+import base.BaseTests;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class FrameTest extends BaseTests {
+	@Test
+	public void testWysiwyg(){
+		String text1 = "hello ";
+		String text2 = "world";
+		var editorPage = homePage.clickWysiwygEditor();
+		editorPage.clearTextArea();
+		editorPage.setTextArea(text1);
+		editorPage.decreaseIndention();
+		editorPage.setTextArea(text2);
+		assertEquals(editorPage.getTextFromEditor(), text1+text2, "Text from editor is incorrect");
+	}
 
 }
