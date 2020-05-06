5423f2f0abae28eff3bad8f241153785eff2f60a
mwaz <amby.mwaz@gmail.com>
2020-05-06 13:02:52 +0300

    fix: beforeTest clause

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -45,4 +45,15 @@ public class HomePage {
 		return new HorizontalSliderPage(driver);
 	}
 
+	public AlertsPage clickJavaScriptAlerts(){
+
+		clickLink("JavaScript Alerts");
+		return new AlertsPage(driver);
+	}
+
+	public FileUploadPage clickFileUpload(){
+		clickLink("File Upload");
+		return new FileUploadPage(driver);
+	}
+
 }
