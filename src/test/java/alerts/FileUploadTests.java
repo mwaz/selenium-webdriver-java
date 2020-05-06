0ad0925c50512fed473290913d5aed8422b6dafe
mwaz <amby.mwaz@gmail.com>
2020-05-06 13:01:49 +0300

    fix: file uploaad tests

--- src/test/java/alerts/FileUploadTests.java
+++ src/test/java/alerts/FileUploadTests.java
@@ -1,5 +1,16 @@
 package alerts;
 
-public class FileUploadTests {
+import base.BaseTests;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class FileUploadTests extends BaseTests {
+	@Test
+	public void testFileUpload(){
+		var uploadPage = homePage.clickFileUpload();
+		uploadPage.uploadFile("/Users/waweruh/Documents/java/java-webdriver/resources/chromedriver");
+		assertEquals(uploadPage.getUploadedFiles(), "chromedriver", "Uploaded files incorrect");
+	}
 
 }
