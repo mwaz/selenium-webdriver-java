f4e8f403ecdae61f9f3c8b1e6ba10f5e206be1cf
mwaz <amby.mwaz@gmail.com>
2020-05-06 13:03:10 +0300

    fix: basetests modification

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
 
 public class BaseTests {
@@ -11,14 +12,20 @@ public class BaseTests {
 	private WebDriver driver;
 	protected HomePage homePage;
 
+	@BeforeMethod
+	public void goHome(){
+		driver.get(Links.URL);
+		homePage = new HomePage(driver);
+	}
+
 	@BeforeClass
 	public void setUp(){
 		System.setProperty("webdriver.chrome.silentOutput", "true");
 		System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
 		driver = new ChromeDriver();
-		driver.get(Links.URL);
-		homePage = new HomePage(driver);
-
+//		driver.get(Links.URL);
+//		homePage = new HomePage(driver);
+		goHome();
 	}
 	@AfterClass
 	public  void TearDown(){
