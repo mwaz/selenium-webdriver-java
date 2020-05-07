9381d5a3c909c66aea734685b9440578008b69b3
mwaz <amby.mwaz@gmail.com>
2020-05-07 09:42:44 +0300

    test: wait tests

--- src/test/java/waits/WaitTests.java
+++ src/test/java/waits/WaitTests.java
@@ -1,5 +1,26 @@
 package waits;
 
-public class WaitTests {
+import base.BaseTests;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+
+public class WaitTests extends BaseTests {
+	@Test
+	public void testWaitUntilHidden(){
+		var loadingPage = homePage.clickDynamicLoading().clickExample1();
+		loadingPage.startButtonClick();
+		assertTrue(loadingPage.getLoadedText().contains("Hello World"), "text not loaded yet");
+
+	}
+
+	@Test
+	public void testWaitUntilAvailable(){
+		var loadingPage = homePage.clickDynamicLoading().clickExample2();
+		loadingPage.startButtonClick();
+		assertTrue(loadingPage.getLoadedText().contains("Hello World"), "text not loaded yet");
+
+	}
 
 }
