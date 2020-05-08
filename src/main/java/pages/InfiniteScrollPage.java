b7ced18684449d0bd6b3f6f3030c758ff41957b2
mwaz <amby.mwaz@gmail.com>
2020-05-08 06:22:55 +0300

    test: implemeenting infinite scroll tests

--- src/main/java/pages/InfiniteScrollPage.java
+++ src/main/java/pages/InfiniteScrollPage.java
@@ -1,5 +1,13 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class InfiniteScrollPage {
+	private WebDriver driver;
+
+	public InfiniteScrollPage(WebDriver driver){
+		this.driver = driver;
+	}
+
 
 }
