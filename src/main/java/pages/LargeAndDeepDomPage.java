2ef25e81c84dad55a64945df977b8293191680aa
mwaz <amby.mwaz@gmail.com>
2020-05-08 06:21:58 +0300

    test: deep dom java test

--- src/main/java/pages/LargeAndDeepDomPage.java
+++ src/main/java/pages/LargeAndDeepDomPage.java
@@ -1,5 +1,26 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class LargeAndDeepDomPage {
+	private WebDriver driver;
+	private By table = By.id("large-table");
+
+
+	public LargeAndDeepDomPage(WebDriver driver){
+		this.driver = driver;
+	}
+
+	public void scrollToTable(){
+		String script = "arguments[0].scrollIntoView();";
+		WebElement tableElement = driver.findElement(table);
+		((JavascriptExecutor)driver).executeScript(script, tableElement);
+	}
+
+
+
 
 }
