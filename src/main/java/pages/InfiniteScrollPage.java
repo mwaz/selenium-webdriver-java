e2ef486494afaba6e5af512fd59a4aeafc3b52a9
mwaz <amby.mwaz@gmail.com>
2020-05-08 11:57:36 +0300

    fix: infinite scroll test

--- src/main/java/pages/InfiniteScrollPage.java
+++ src/main/java/pages/InfiniteScrollPage.java
@@ -1,12 +1,28 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 
 public class InfiniteScrollPage {
 	private WebDriver driver;
+	private By textBlocks = By.className("jscroll-added");
 
 	public InfiniteScrollPage(WebDriver driver){
 		this.driver = driver;
+	}
+
+	public void scrollToParagraph(int index){
+		var jsExecutor = (JavascriptExecutor)driver;
+		String script = "window.scrollTo(0, document.body.scrolleight)";
+		while(getNumberOfParagraphsPresent() < index){
+			jsExecutor.executeScript(script);
+
+		}
+	}
+
+	private int getNumberOfParagraphsPresent(){
+		return driver.findElements(textBlocks).size();
 	}
 
 
